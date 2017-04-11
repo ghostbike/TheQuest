ca3a3e818d296ff1114e0a8a3a1c6804cb4bb393
ghostbike <krysmathis@gmail.com>
2017-04-10 20:43:57 -0500

    Initial Commit

---
+++ TheQuest/Ghost.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace TheQuest
+{
+    class Ghost : Enemy
+    {
+        public Ghost(Game game, Point location) :
+            base(game, location, 8)
+        { }
+
+        public override void Move(Random random)
+        {
+            Console.WriteLine("Ghost location: {0}", this.location.ToString());
+            bool moveTowardPlayer = false;
+            if (random.Next(0, 100) < 50)
+            {
+                moveTowardPlayer = true;
+            }
+
+            if (moveTowardPlayer)
+                location = Move(FindPlayerDirection(game.PlayerLocation), game.Boundaries);
+            else
+                location = Move((Direction)random.Next(1, 4), game.Boundaries);
+
+            if (NearPlayer())
+            {
+                game.HitPlayer(2, random);
+            }
+        }
+    }
+}
